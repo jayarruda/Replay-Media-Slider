24d185c821048cd3d7650e10e79c99b501ff725f
G-grbz <127258824+G-grbz@users.noreply.github.com>
2025-08-16 21:36:48 +0300

    Update api.js

--- modules/api.js
+++ modules/api.js
@@ -262,6 +262,7 @@ async function getRandomEpisodeId(seriesId) {
 
 export async function getVideoStreamUrl(
   itemId,
+  maxHeight = 360,
   startTimeTicks = 0,
   audioLanguage = null,
   preferredVideoCodecs = ["hevc", "h264", "av1"],
@@ -422,7 +423,9 @@ if (item.Type === "Series") {
         api_key: accessToken,
         VideoCodec: "h264",
         AudioCodec: "aac",
+        VideoBitrate: 1000000,
         AudioBitrate: 128000,
+        MaxHeight: maxHeight,
         StartTimeTicks: startTimeTicks
       };
 
@@ -445,6 +448,9 @@ if (item.Type === "Series") {
       api_key: accessToken,
       VideoCodec: videoCodec,
       AudioCodec: audioCodec,
+      VideoBitrate: 1000000,
+      AudioBitrate: 128000,
+      MaxHeight: maxHeight,
       StartTimeTicks: startTimeTicks,
       AudioStreamIndex: audioStreamIndex
     };
@@ -479,7 +485,7 @@ export async function getIntroVideoUrl(itemId) {
     if (intros.length > 0) {
       const intro = intros[0];
       const startTimeTicks = 600 * 10_000_000;
-      const url = await getVideoStreamUrl(intro.Id, startTimeTicks);
+      const url = await getVideoStreamUrl(intro.Id, 360, startTimeTicks);
       return url;
     }
     return null;
@@ -496,7 +502,7 @@ export async function getCachedVideoPreview(itemId) {
     return videoPreviewCache.get(itemId);
   }
 
-  const url = await getVideoStreamUrl(itemId, 0);
+  const url = await getVideoStreamUrl(itemId, 360, 0);
   if (url) {
     videoPreviewCache.set(itemId, url);
     setTimeout(() => videoPreviewCache.delete(itemId), 300000);
